861bc8469a736f27df1d151b52df0f70d9108df4
nguyenhoanganhtuan1206 <nguyenhanhtuan1206@gmail.com>
2023-03-02 11:10:48 +0700

    Fix and add dependency for swagger

--- src/main/java/com/javatemplate/api/book/BookController.java
+++ src/main/java/com/javatemplate/api/book/BookController.java
@@ -1,6 +1,6 @@
 package com.javatemplate.api.book;
 
-import com.javatemplate.domain.book.BookService;
+import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -8,17 +8,14 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
-import static com.javatemplate.api.book.BookDTOMapper.toBookDTOs;
-
 @RestController
-@RequestMapping(value = "/api/v1/books")
+@RequestMapping("api/v1/books")
 @RequiredArgsConstructor
 public class BookController {
 
-    private final BookService bookService;
-
+    @Operation(summary = "Find all available books")
     @GetMapping
     public List<BookDTO> findAll() {
-        return toBookDTOs(bookService.findAll());
+        return null;
     }
 }
--- src/main/java/com/javatemplate/api/book/BookDTO.java
+++ src/main/java/com/javatemplate/api/book/BookDTO.java
@@ -10,5 +10,6 @@ import java.util.UUID;
 public class BookDTO {
 
     private UUID id;
+
     private String name;
 }
