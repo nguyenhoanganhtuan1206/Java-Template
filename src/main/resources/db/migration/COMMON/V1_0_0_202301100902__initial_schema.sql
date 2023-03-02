41e47c770eb15f9f04de6a93129e6d9f271bf7bb
nguyenhoanganhtuan1206 <nguyenhanhtuan1206@gmail.com>
2023-03-02 15:36:06 +0700

    Fix migration

--- src/main/resources/db/migration/COMMON/V1_0_0_202301100902__initial_schema.sql
+++ src/main/resources/db/migration/COMMON/V1_0_0_202301100902__initial_schema.sql
@@ -6,7 +6,7 @@ CREATE TABLE books
     name VARCHAR(255) NOT NULL,
     author VARCHAR(255) NOT NULL,
     description VARCHAR(255),
-    created_at TIMESTAMP NOT NULL,
+    created_at TIMESTAMP NOT NULL DEFAULT NOW(),
     updated_at TIMESTAMP,
     image VARCHAR(255),
     user_id INT NOT NULL,
--- src/main/resources/db/migration/DEV/V1_0_0_202301100904__add_sample_users.sql
+++ src/main/resources/db/migration/DEV/V1_0_0_202301100904__add_sample_users.sql
@@ -1,5 +1,5 @@
 INSERT INTO users (id, username, password, firstName, lastName, enabled, avatar, role_id)
 VALUES
-    (1, 'admin', '123456', 'Nguyen H', 'A Tuan', true,null, 1),
-    (2, 'Anh Tuan', '123123', 'Nguyen', 'Tuan', true, null, 2),
-    (3, 'Anh Tuan 1', '12123',  'Nguyen', 'A Tuan', true, null, 2);
+    (1, 'admin', 'vhga65tQgga+rlGKUKAxTJEmnDqOePeQ+0+Y3vOgLwo=', 'Nguyen H', 'A Tuan', true,null, 1),
+    (2, 'Anh Tuan', 'OHjcWdaFE35A7kBJBmVyjlNfaJ8lWgHI2zaUR4tjGhI=', 'Nguyen', 'Tuan', true, null, 2),
+    (3, 'Anh Tuan 1', 'OHjcWdaFE35A7kBJBmVyjlNfaJ8lWgHI2zaUR4tjGhI=',  'Nguyen', 'A Tuan', true, null, 2);
