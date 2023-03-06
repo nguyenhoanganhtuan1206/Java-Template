ae8a8cbca1c54def73218a6bbb73558aed9d2ddb
nguyenhoanganhtuan1206 <nguyenhanhtuan1206@gmail.com>
2023-03-06 11:24:34 +0700

    Fix

--- src/test/java/com/javatemplate/utils/TestUtils.java
+++
@@ -1,9 +0,0 @@
-package com.javatemplate.utils;
-
-import lombok.experimental.UtilityClass;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-
-@UtilityClass
-@AutoConfigureMockMvc
-public class TestUtils {
-}
