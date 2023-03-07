cef6d8cacd42fc7b23ef3b98e7921252703ae695
nguyenhoanganhtuan1206 <nguyenhanhtuan1206@gmail.com>
2023-03-07 14:52:39 +0700

    first commit

---
+++ src/main/java/com/javatemplate/api/auth/AuthController.java
@@ -0,0 +1,9 @@
+package com.javatemplate.api.auth;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/auths")
+public class AuthController {
+}
