903d79b48255a31921d71d4e33de6ac57a33b9f4
nguyenhoanganhtuan1206 <nguyenhanhtuan1206@gmail.com>
2023-03-23 14:45:11 +0700

    Cover codecov

--- src/main/java/com/javatemplate/api/auth/LoginDTO.java
+++ src/main/java/com/javatemplate/api/auth/LoginDTO.java
@@ -3,10 +3,8 @@ package com.javatemplate.api.auth;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
-import lombok.Setter;
 
 @Getter
-@Setter
 @Builder
 @AllArgsConstructor
 public class LoginDTO {
