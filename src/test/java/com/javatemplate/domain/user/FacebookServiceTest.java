d368ecab5df5f217424d6831ede06f7488b57e8a
nguyenhoanganhtuan1206 <nguyenhanhtuan1206@gmail.com>
2023-03-31 22:53:03 +0700

    Fix codecov

--- src/test/java/com/javatemplate/domain/user/FacebookServiceTest.java
+++ src/test/java/com/javatemplate/domain/user/FacebookServiceTest.java
@@ -2,9 +2,19 @@ package com.javatemplate.domain.user;
 
 import org.junit.jupiter.api.Test;
 
+import static org.apache.commons.lang3.RandomStringUtils.randomAlphabetic;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
 class FacebookServiceTest {
 
     @Test
-    void shouldParseToken_OK() {
+    public void testSocialUserCreation() {
+        final SocialUser socialUserWithEmail = SocialUser.builder()
+                .firstName(randomAlphabetic(6, 10))
+                .lastName(randomAlphabetic(6, 10))
+                .username(randomAlphabetic(6, 10)).build();
+        assertNotNull(socialUserWithEmail.getFirstName());
+        assertNotNull(socialUserWithEmail.getLastName());
+        assertNotNull(socialUserWithEmail.getUsername());
     }
 }
