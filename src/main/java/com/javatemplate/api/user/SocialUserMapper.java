1439df03fd5dea454bd95726dd05f59e9e492199
nguyenhoanganhtuan1206 <nguyenhanhtuan1206@gmail.com>
2023-04-02 14:53:43 +0700

    Fix codecov - fix logic

---
+++ src/main/java/com/javatemplate/api/user/SocialUserMapper.java
@@ -0,0 +1,17 @@
+package com.javatemplate.api.user;
+
+import com.javatemplate.domain.user.SocialUser;
+import com.javatemplate.domain.user.User;
+import lombok.experimental.UtilityClass;
+
+@UtilityClass
+public class SocialUserMapper {
+
+    public static SocialUser toSocialUser(final User user) {
+        return SocialUser.builder()
+                .username(user.getUsername())
+                .firstName(user.getFirstName())
+                .lastName(user.getLastName())
+                .build();
+    }
+}
