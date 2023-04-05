291a28801c5db49f4f717b6d5cf51568ba345c3d
nguyenhoanganhtuan1206 <nguyenhanhtuan1206@gmail.com>
2023-04-05 14:45:28 +0700

    Fix

--- src/main/java/com/javatemplate/domain/auth/JwtUserDetailsService.java
+++ src/main/java/com/javatemplate/domain/auth/JwtUserDetailsService.java
@@ -1,17 +1,14 @@
 package com.javatemplate.domain.auth;
 
-import com.javatemplate.domain.role.Role;
 import com.javatemplate.error.UsernameNotFoundException;
 import com.javatemplate.persistent.role.RoleStore;
 import com.javatemplate.persistent.user.UserStore;
 import lombok.RequiredArgsConstructor;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
-import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.stereotype.Service;
 
-import java.util.List;
+import static com.javatemplate.domain.auth.UserDetailsMapper.toUserDetails;
 
 @Service
 @RequiredArgsConstructor
@@ -24,18 +21,7 @@ public class JwtUserDetailsService implements UserDetailsService {
     @Override
     public UserDetails loadUserByUsername(final String username) throws UsernameNotFoundException {
         return userStore.findByUsername(username)
-                .map(this::buildUser)
+                .map(user -> toUserDetails(user, roleStore.findById(user.getRoleId()).getName()))
                 .orElseThrow(() -> new UsernameNotFoundException(username));
-    }
-
-    private User buildUser(final com.javatemplate.domain.user.User user) {
-        final Role role = roleStore.findById(user.getRoleId());
-
-        return new JwtUserDetails(
-                user.getId(),
-                user.getUsername(),
-                user.getPassword(),
-                List.of(new SimpleGrantedAuthority(role.getName()))
-        );
     }
 }
---
+++ src/main/java/com/javatemplate/domain/auth/UserDetailsMapper.java
@@ -0,0 +1,20 @@
+package com.javatemplate.domain.auth;
+
+import lombok.experimental.UtilityClass;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collections;
+
+@UtilityClass
+public class UserDetailsMapper {
+
+    public static UserDetails toUserDetails(final com.javatemplate.domain.user.User user, final String roleName) {
+        return new JwtUserDetails(
+                user.getId(),
+                user.getUsername(),
+                user.getPassword(),
+                Collections.singleton(new SimpleGrantedAuthority(roleName))
+        );
+    }
+}
